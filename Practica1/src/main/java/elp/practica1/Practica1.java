2e1a3a797e8d5d4cfb3028958c497a34e3b95309
elp <leblicqe@gmail.com>
2024-06-15 12:52:11 -0600

    Codigo inicial

---
+++ Practica1/src/main/java/elp/practica1/Practica1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package elp.practica1;
+
+/**
+ *
+ * @author elp51
+ */
+public class Practica1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
