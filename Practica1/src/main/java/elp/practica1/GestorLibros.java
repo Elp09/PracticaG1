ce6177388b9df1201cdb92a8883f760d3bef1146
elp <leblicqe@gmail.com>
2024-06-16 17:10:15 -0600

    GestorLibros

--- Practica1/src/main/java/elp/practica1/GestorLibros.java
+++ Practica1/src/main/java/elp/practica1/GestorLibros.java
@@ -17,12 +17,41 @@ public class GestorLibros {
     }
     
     public void eliminar_libro(String id){
+        boolean removed = false;
         for (Libro libro : libros) {
             if (libro.getId().equals(id)){
+                removed = true;
                 libros.remove(libro);
             }
         }
+        if (!removed){
+            System.out.println("No se elimino ningun libro");
+        }
+            
     }
+    
+    public void buscar_libro(String id){
+        boolean encontrado = false;
+        for (Libro libro : libros) {
+            if (libro.getId().equals(id)){
+                encontrado = true;
+                System.out.println("Se encontro el libro: " + libro);
+            }
+        }
+        if (!encontrado){
+            System.out.println("No se encontro ningun libro");
+        }
+    }
+
+    public ArrayList<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(ArrayList<Libro> libros) {
+        this.libros = libros;
+    }
+    
+    
     
     
 }
--- Practica1/src/main/java/elp/practica1/Practica1.java
+++ Practica1/src/main/java/elp/practica1/Practica1.java
@@ -6,13 +6,23 @@ package elp.practica1;
 public class Practica1 {
 
     public static void main(String[] args) {
+        GestorLibros libros = new GestorLibros();
         Autor autor1 = new Autor("pablo", "A-01");
         Autor autor2 = new Autor("pedro", "A-02");
-        Libro libro1 = new Libro("Matilda", "L-01");
+        Libro libro1 = new Libro("L-01","Matilda" );
+        Libro libro2 = new Libro("L-02", "Popeye" );
+        
         libro1.anadir_autor(autor1);
         libro1.anadir_autor(autor2);
-        System.out.println(libro1);
-        System.out.println(libro1.getAutores());
+        
+        
+
+        
+        libros.anadir_libro(libro1);
+        libros.anadir_libro(libro2);
+        
+        System.out.println(libros.getLibros());
+        libros.buscar_libro("L-02");
     }
     
     
