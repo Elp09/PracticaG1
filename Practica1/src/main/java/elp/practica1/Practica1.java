85ca5e271ceb5f737b768bc68fb988e367c580e5
vjndas <Santiagovc590@gmail.com>
2024-06-17 17:50:22 -0600

    Ultimo arreglo de todo

--- Practica1/src/main/java/elp/practica1/Practica1.java
+++ Practica1/src/main/java/elp/practica1/Practica1.java
@@ -15,9 +15,6 @@ public class Practica1 {
         Libro libro1 = new Libro("L-01","Matilda" );
         Libro libro2 = new Libro("L-02", "Popeye" );
         
-        Menu menu1 = new Menu();
-        
-        menu1.menu();
         
         libro1.anadir_autor(autor1);
         libro1.anadir_autor(autor2);
@@ -25,9 +22,11 @@ public class Practica1 {
         prestamos.registrar_prestamo("L-01", "U-01");
         prestamos.mostrar_prestamos();
         prestamos.registrar_prestamo("L-01", "U-01");
+        Menuu menu01 = new Menuu();
+        
+        menu01.menu();
         
         
-        System.out.println("esto es un ejemplo nada mas para ver como se hace el commit en git :))");
     }
     
     
